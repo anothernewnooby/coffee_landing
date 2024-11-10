b2bc9e7a61e8043357c53699d8f6baf2ee704b0c
anothernewnooby <142803888+anothernewnooby@users.noreply.github.com>
2024-11-10 00:10:03 -0300

    asds

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
-// astro.config.mjs
+// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  // Configura el adaptador para el despliegue en Vercel
-  output: 'static',
-  // Configura el adaptador para desplegar en Vercel
-  integrations: [],
-});
+export default defineConfig({});
